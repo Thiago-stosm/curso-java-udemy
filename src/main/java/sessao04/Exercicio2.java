a501cdbc7cf306e500e9e85ec86e1dd15cd01139
Thiago <thiagosm2006@gmail.com>
2025-06-14 19:29:05 -0300

    Sessão 4 (Exercício 2)

--- src/main/java/sessao04/Exercicio2.java
+++ src/main/java/sessao04/Exercicio2.java
@@ -1,4 +1,21 @@
 package sessao04;
 
+import java.util.Scanner;
+/*Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+casas decimais conforme exemplos.*/
+
 public class Exercicio2 {
+    public static void main(String [] args){
+
+    // Declaração da variável para uso de Pi
+    double pi =  3.14159;
+
+    // Entrada de dados e processamento
+    Scanner sc = new Scanner(System.in);
+    double raio = sc.nextDouble();
+    double area = (pi * (Math.pow(raio, 2)));
+
+    // Saída de dados
+    System.out.printf("A área do círculo em questão é igual a: A = %.4f", area);
+    }
 }
