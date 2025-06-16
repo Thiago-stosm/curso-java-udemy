9520baa33ad36410fc5e3e26fc8c1f85ebeaa18a
Thiago <thiagosm2006@gmail.com>
2025-06-15 22:25:31 -0300

    Sessão 6 (Exercício 1)

---
+++ src/main/java/sessao06/Exercicio1.java
@@ -0,0 +1,21 @@
+package sessao06;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main (String [] args){
+        
+        Scanner sc = new Scanner(System.in);
+        // Validação de senha
+        int senha = 2002;
+        int num = 0;
+        while (num != senha){
+            num = sc.nextInt();
+            if (senha != num){
+                System.out.println("Senha incorreta!");
+            }
+            }
+        }
+
+    }
+
