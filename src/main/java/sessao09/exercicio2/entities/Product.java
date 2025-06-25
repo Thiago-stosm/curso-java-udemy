8a83c40943f09fa87a5e9d64b32b7b4ca0c1d421
Thiago <thiagosm2006@gmail.com>
2025-06-24 21:57:52 -0300

    Sessão 9 (Atualizações do Exercício 2)

---
+++ src/main/java/sessao09/exercicio2/entities/Product.java
@@ -0,0 +1,52 @@
+package sessao09.exercicio2.entities;
+
+public class Product {
+
+    private final int NUMBER;
+    private final double PRICE;
+    private String name;
+    private int stock;
+
+    public Product(int NUMBER, double PRICE, String name){
+        this.NUMBER = NUMBER;
+        this.PRICE = PRICE;
+        this.name = name;
+    }
+    public Product(int NUMBER, double PRICE, String name, int initialQtd){
+        this.NUMBER = NUMBER;
+        this.PRICE = PRICE;
+        this.name = name;
+        this.addProduct(initialQtd);
+    }
+
+    // Getters e Setters
+    public int getNUMBER(){
+        return this.NUMBER;
+    }
+    public double getPRICE(){
+        return this.PRICE;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String setName(String name){
+        return this.name;
+    }
+    public int getStock(){
+        return this.stock;
+    }
+
+    // Métodos especiais
+    public void addProduct(int productAmount){
+        this.stock += productAmount;
+    }
+    public void subProduct(int productAmount){
+        this.stock -= productAmount;
+    }
+    public String toString(){
+        return "Product Name: " + getName() + "\n" +
+                "Product Price: " + getPRICE() + "\n" +
+                "Product Stock: $" + String.format("%d", getStock());
+    }
+
+}
---
+++ src/main/java/sessao09/exercicio2/main/Main.java
@@ -0,0 +1,13 @@
+package sessao09.exercicio2.main;
+
+import sessao09.exercicio2.entities.Product;
+import java.util.*;
+
+public class Main {
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        Product product;
+
+
+    }
+}
