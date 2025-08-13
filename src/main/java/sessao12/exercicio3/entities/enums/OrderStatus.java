7af4379143cb510570062ae77a943d0b22b9544d
Thiago <thiagosm2006@gmail.com>
2025-08-13 11:29:49 -0300

    feat: classe OrderStatus finalizada

--- src/main/java/sessao12/exercicio3/entities/enums/OrderStatus.java
+++ src/main/java/sessao12/exercicio3/entities/enums/OrderStatus.java
@@ -1,4 +1,18 @@
 package sessao12.exercicio3.entities.enums;
 
 public enum OrderStatus {
+    PENDING_PAYMENT(0),
+    PROCESSING (1),
+    SHIPPED(2),
+    DELIVERED(3);
+
+    private final int codigo;
+
+    OrderStatus(int codigo){
+        this.codigo = codigo;
+    }
+
+    public int getCodigo(){
+        return this.codigo;
+    }
 }
